65c08ae1507ff11656fe5f6f2faa797a97b82711
default <2290957226@qq.com>
2023-10-03 04:29:49 -0400

    根据ymal文件部署deployment类型

---
+++ src/scheduler/deployment_create.py
@@ -0,0 +1,19 @@
+from os import path
+
+import yaml
+
+from kubernetes import client, config
+
+def create_deployment():
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
+
+    with open(path.join(path.dirname(path.dirname(path.dirname(__file__))), "config/yaml/test.yaml")) as f:
+        dep = yaml.safe_load(f)
+        k8s_apps_v1 = client.AppsV1Api()
+        resp = k8s_apps_v1.create_namespaced_deployment(
+            body=dep, namespace="default")
+        print(f"Deployment created. Status='{resp.metadata.name}'")
+
